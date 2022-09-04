e0fc2d126b2b995f15da180f67ab6c60d5dd78b3
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-09-04 13:31:00 +0530

    set Operations

--- arrays/setOperation.cpp
+++ arrays/setOperation.cpp
@@ -35,8 +35,56 @@ struct Array *Union(struct Array *arr1, struct Array *arr2)
     for (; j < arr2->length; j++)
         arr3->A[k++] = arr2->A[j];
 
-arr3->length = k;
-arr3->size=10;
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array *Intersection(struct Array *arr1, struct Array *arr2)
+{
+    int i = 0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else if (arr1->A[i] == arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array *Difference(struct Array *arr1, struct Array *arr2)
+{
+    int i = 0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    arr3->length = k;
+    arr3->size = 10;
 
     return arr3;
 }
@@ -47,7 +95,7 @@ int main()
     struct Array arr2 = {{2, 3, 9, 18, 28}, 10, 5};
     struct Array *arr3;
 
-    arr3 = Union(&arr1, &arr2);
+    arr3 = Difference(&arr1, &arr2);
     display(*arr3);
 
     return 0;
