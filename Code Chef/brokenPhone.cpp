2f8a7d1a3f2bd0be019c5bcca1ec5a0acd17bc2f
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-09-07 20:15:26 +0530

    Problem Code: BROKENPHONE

---
+++ Code Chef/brokenPhone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x, y;
+	    cin>>x>>y;
+	    if(x<y)
+	    cout<<"REPAIR\n";
+	    else if(x>y)
+	    cout<<"NEW PHONE\n";
+	    else if(x==y)
+	    cout<<"ANY\n";
+	}
+	return 0;
+}
