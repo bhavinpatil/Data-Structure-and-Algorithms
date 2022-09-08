74abef71453bc029bb78dc2a51b7d17880206010
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-09-08 21:51:02 +0530

    Problem Code: MEANMEDIAN

--- Code Chef/meanAndMedian.cpp
+++ Code Chef/meanAndMedian.cpp
@@ -9,3 +9,17 @@ int main() {
     }
 	return 0;
 }
+
+/*======================OR====================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int x, y; cin>>x>>y;
+        cout<<y<<" "<<y<<" "<<3*x-2*y<<endl;
+    }
+	return 0;
+}
