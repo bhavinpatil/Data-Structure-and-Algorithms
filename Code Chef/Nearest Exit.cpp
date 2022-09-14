61bac75a6756911f2531cef88eb3f0bed8661674
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-09-14 20:26:10 +0530

    Starters 56 Problem Code: NEARESTEXIT

---
+++ Code Chef/Nearest Exit.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t;
+	while(t--)
+	{
+	    int x; cin>>x;
+	    if(x>50)
+	    {
+	        cout<<"RIGHT\n";
+	    }
+	    else
+	    {
+	        cout<<"LEFT\n";
+	    }
+	}
+	return 0;
+}
