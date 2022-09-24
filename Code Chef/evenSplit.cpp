c6252b64aa8160e1d8942c4625d110d78e8c3d68
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-09-24 15:07:43 +0530

    Starters 57 Problem Code: EVENSPLIT

---
+++ Code Chef/evenSplit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    string s; cin>>s;
+	    
+	    if(n<=2) cout<<s<<endl;
+	    else{
+	        sort(s.begin(),s.end());
+	        cout<<s<<endl;
+	    }
+	}
+	return 0;
+}
