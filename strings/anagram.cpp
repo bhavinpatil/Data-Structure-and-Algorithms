f6b6c715b3689e89b8f082fd937d18f116cc98b9
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-10-02 18:25:12 +0530

    strings

---
+++ strings/anagram.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char A[] = "mdical";
+    char B[] = "decimal";
+    int i, H[26] = {0};
+    for (i = 0; A[i] != '\0'; i++)
+        H[A[i] - 97]+=1;
+
+    for (i = 0; B[i] != '\0'; i++)
+    {
+        H[A[i] - 97]-=1;
+        if (H[A[i] - 97] < 0)
+        {
+            cout << "Not Anagram\n";
+            break;
+        }
+    }
+    if (B[i] == '\0')
+        cout << "Anagram!!!\n";
+
+    return 0;
+}
