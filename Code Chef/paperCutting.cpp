4a11ad76053df3eaf4465dceca75e6d76104feb1
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-10-12 20:53:59 +0530

    Paper Cutting Problem Code: CUTPAPER

---
+++ Code Chef/paperCutting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t;
+	while(t--)
+	{
+	    int n, k; cin>>n>>k;
+	    int c=n/k;
+	    cout<<c*c<<endl;
+	   // if(n==k) cout<<"1";
+	   // int sn = n*n;
+	   // int sk = k*k;
+	   // int countSq=0;
+	   // while(sn>=sk)
+	   // {
+	   //     sn -= sk;
+	   //     countSq++;
+	        
+	   // }
+	   // cout<<countSq<<endl;
+	}
+	return 0;
+}
