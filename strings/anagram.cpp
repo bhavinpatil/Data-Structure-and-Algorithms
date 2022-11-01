c2a421c3b10e432e8cbcdec6bf05aca14636ba5a
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-11-01 17:48:21 +0530

    strings completed

--- strings/anagram.cpp
+++ strings/anagram.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    char A[] = "mdical";
+    char A[] = "medical";
     char B[] = "decimal";
     int i, H[26] = {0};
     for (i = 0; A[i] != '\0'; i++)
-        H[A[i] - 97]+=1;
+        H[A[i] - 97]++;
 
     for (i = 0; B[i] != '\0'; i++)
     {
-        H[A[i] - 97]-=1;
-        if (H[A[i] - 97] < 0)
+        H[A[i] - 97]--;
+        if (H[A[i] - 97] < 0 || H[A[i] - 97] > 0)
         {
             cout << "Not Anagram\n";
             break;
--- strings/comparingString.cpp
+++ strings/comparingString.cpp
@@ -1,19 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    char A[] = "Darth Sidious";
+    char A[] = "Darth Vader";
     char B[] = "Darth Vader";
     int i = 0, j = 0;
-    while(A[i] && B[j])
+    while (A[i] && B[j])
     {
-        if(A[i]!=B[j]) break;
+        if (A[i] != B[j])
+            break;
         i++, j++;
     }
-    if(A[i]==B[j]) cout<<"Equal!"<<endl;
-    else if(A[i]>B[j]) cout<<"1st string is greater than 2nd string"<<endl;
-    else cout<<"2st string is greater than 1nd string"<<endl;
+    if (A[i] == B[j])
+        cout << "Equal!" << endl;
+    else if (A[i] > B[j])
+        cout << "1st string is greater than 2nd string" << endl;
+    else
+        cout << "2st string is greater than 1nd string" << endl;
 
     return 0;
 }
